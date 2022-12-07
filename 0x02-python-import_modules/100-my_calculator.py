cea4bdd241c9d5cec5ac4250b63eb6b675d737ba
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-12-07 01:14:26 -0800

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-
-
 if __name__ == "__main__":
 
     """Handle basic arithmetic operations"""
@@ -10,15 +8,11 @@ if __name__ == "__main__":
 
     import sys
 
-
-
     if len(sys.argv) - 1 != 3:
 
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
 
         sys.exit(1)
-
-
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
 
@@ -27,8 +21,6 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
 
         sys.exit(1)
-
-
 
     a = int(sys.argv[1])
 
