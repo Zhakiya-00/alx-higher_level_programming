35a9b8f7fee0fe5d87136eab48c947b6a9844130
zhakiya-00 <zakiyayusuf123@gmail.com>
2022-12-07 01:05:03 -0800

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,10 +2,14 @@
 
 if __name__ == "__main__":
 
+    """Print all names defined by hidden_4 module."""
+
     import hidden_4
 
-    for s in dir(hidden_4):
+    names = dir(hidden_4)
 
-        if s[0:2] != "__":
+    for name in names:
 
-            print(s)
+        if name[:2] != "__":
+
+            print(name)
