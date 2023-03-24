f27ce703f7183068c82f3ec53af5b8ec1210a18c
Your Name <you@example.com>
2023-03-24 13:08:40 +0000

    states 2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-''' Select states and order by ID'''
+""" script that lists all states from the database hbtn_0e_0_usa """
 
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    try:
-        usr = sys.argv[1]
-        passwrd = sys.argv[2]
-        db_name = sys.argv[3]
-        db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=usr,
-            passwd=passwrd,
-            db=db_name
-            )
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-        cur.close()
-        db.close()
-    except Exception as e:
-        print(e)
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
